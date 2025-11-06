8b8a8f136ad713b1a27032cc4f2e9a7cd0a9d6b8
Maurokings <morito1402@gmail.com>
2025-11-06 02:28:25 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app import modelos, crud
+from app import modelos, crud, auth
 from app.database import engine, Base
-from app.routers import auth, barberos, servicios, cortes, gastos
+from app.routers import barberos, servicios, cortes, gastos
 
 # Crear las tablas en la base de datos si no existen
 Base.metadata.create_all(bind=engine)
